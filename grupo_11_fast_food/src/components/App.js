1f37fb6a2b2cc31a9c59197ffac8d8e66875c544
deoma10 <danielospinam@hotmail.com>
2022-02-23 18:51:54 -0500

    construccion del sideBar

--- grupo_11_fast_food/src/components/App.js
+++ grupo_11_fast_food/src/components/App.js
@@ -1,10 +1,12 @@
 import '../assets/css/App.css';
 import NavBar from './navBar/navBar'
 import MainContent from './mainContent/mainContent';
+import SideBar from ./sideBar/SideBar';
 
 function App() {
   return (
     <div className='main'>
+      <SideBar/>
       <NavBar />
       <MainContent />
     </div>
---
+++ grupo_11_fast_food/src/components/sideBar/sideBar.js
@@ -0,0 +1,13 @@
+import '../../assets/css/sideBar.css'
+import { Link } from 'react-router-dom';
+
+function SideBar(props) {
+    return (
+        <div className="side-bar">
+            <figure></figure>
+            <p>skjkjrsjkrkjnjk</p>
+        </div>
+    )
+}
+
+export default SideBar;
