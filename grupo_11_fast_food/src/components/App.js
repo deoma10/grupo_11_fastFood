a30fdef5030d0dab99d9cfc66e104cdfb7985c1f
deoma10 <danielospinam@hotmail.com>
2022-02-23 19:42:14 -0500

    Estilos del sideBar

--- grupo_11_fast_food/src/components/App.js
+++ grupo_11_fast_food/src/components/App.js
@@ -2,18 +2,15 @@ import '../assets/css/App.css';
 import SideBar from './sideBar/sideBar';
 import NavBar from './navBar/navBar';
 import MainContent from './mainContent/mainContent';
-import SideBar from ./sideBar/SideBar';
+
 
 function App() {
   return (
     <div className='main'>
-<<<<<<< HEAD
-      <SideBar/>
-=======
-      <SideBar />
->>>>>>> 6cf0856025987b227afc1862387c81740c881e3d
-      <NavBar />
-      <MainContent />
+     <div className='contenedor-main'>
+        <SideBar />
+        <MainContent />
+     </div >
     </div>
   );
 }
--- grupo_11_fast_food/src/components/sideBar/sideBar.js
+++ grupo_11_fast_food/src/components/sideBar/sideBar.js
@@ -1,30 +1,27 @@
-<<<<<<< HEAD
-import '../../assets/css/sideBar.css'
-import { Link } from 'react-router-dom';
-
-function SideBar(props) {
-    return (
-        <div className="side-bar">
-            <figure></figure>
-            <p>skjkjrsjkrkjnjk</p>
-=======
 import logo from '../../assets/images/Logo-Fast-Food.png'
-import '../../assets/css/NavBar.css';
+import '../../assets/css/sideBar.css';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
-        <div className="bar">
-            <Link to="/">
-                <img src={logo} alt="Logo" />
+        <div className="side-bar">
+            <Link to="/" className="side-bar__link">
+                <img  className="side-bar__logo"src={logo} alt="Logo" />
             </Link>
->>>>>>> 6cf0856025987b227afc1862387c81740c881e3d
+            <ul className="side-bar__list">
+                        <li className='side-barItem'>
+                            <Link className='side-barLink' to="/products">
+                                Productos
+                            </Link>
+                        </li>
+                        <li className='side-barItem'>
+                            <Link className='side-barLink' to="/users">
+                                Usuarios
+                            </Link>
+                        </li>
+                    </ul>
         </div>
     )
 }
 
-<<<<<<< HEAD
-export default SideBar;
-=======
 export default NavBar;
->>>>>>> 6cf0856025987b227afc1862387c81740c881e3d
