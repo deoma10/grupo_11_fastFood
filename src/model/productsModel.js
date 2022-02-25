ab756e570c06ced423ed5f80ee55af2f5f094ba5
deoma10 <danielospinam@hotmail.com>
2022-02-24 22:08:14 -0500

    Agrego mas productos a la pagina principal con descripciones

--- src/model/productsModel.js
+++ src/model/productsModel.js
@@ -17,9 +17,9 @@ const productsModel = {
     getOneProduct: async function (value) {
         try {
             let allProducts = await db.products.findAll();
-            if(value <= 0 || value > allProducts.length) {
-                new error
-            }
+            // if(value <= 0 || value > allProducts.length) {
+            //     new error
+            // }
             let product = await db.products.findByPk(value, {
                 include: [{ association: 'fk_idImage_image' }]
             });
