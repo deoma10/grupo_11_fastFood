71ba9a0acab3756fff92d62a1b4cd96cceaa9057
deoma10 <danielospinam@hotmail.com>
2022-02-25 22:56:55 -0500

    Menu lateral para mobile

---
+++ public/js/headerLogic.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function () {
+    let cintaOpciones = document.querySelector('.header__burgermenu');
+    let pBurgerMenu = document.querySelector('.header-mobile-touch__x');
+    console.log('kajkaefjefakefjnkanfaek')
+    cintaOpciones.addEventListener('touchend', function(){
+        menuLateral.classList.remove('header-mobile-touch-close')
+    })
+
+    pBurgerMenu.addEventListener('touchend', function(){
+        menuLateral.classList.add('header-mobile-touch-close')
+    })
+  });
